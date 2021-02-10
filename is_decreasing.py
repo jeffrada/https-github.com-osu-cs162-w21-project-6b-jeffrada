04d8050c0ef3bb9edfc58810c482895549486848
Adam Jeffries <ajlc2012@yahoo.com>
2021-02-10 15:16:17 -0500

    Commit 2

--- is_decreasing.py
+++ is_decreasing.py
@@ -3,11 +3,11 @@
 # Description: recursive function named is_decreasing that takes as its parameter a list of numbers.
 # It should return True if the elements of the list are strictly decreasing but returns False otherwise.
 
-def is_descending(list):
+def is_decreasing(list):
     if len(list) <= 1 or (len(list) == 2 and list[0] > list[1]):
         return True
     else:
         if list[0] > list[1]:
-            return is_descending(list[1::])
+            return is_decreasing(list[1::])
         else:
             return False
